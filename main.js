2a49ef80d828b3ee687d9def24f78db9402d2bcf
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-10-20 10:44:51 -0600

    awd

--- main.js
+++ main.js
@@ -6,19 +6,69 @@ let ctx = cnv.getContext("2d");
 cnv.width = 400;
 cnv.height = 400;
 
-requestAnimationFrame(assignment1)
-function assignment1() {
-    ctx.clearRect(0, 0, cnv.width, cnv.height);
-    ctx.fillStyle = "blue";
-    ctx.fillRect(0, 0, 1000, 300);
-    ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.arc(200, 300, 15, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, 300, 1000, 100);
-    ctx.drawImage(cloudImg, 150, 125);
-    ctx.drawImage(cloudImg, 175, 100);
+let x = 150;
+let x2 = 175;
+let sunY = 300;
+let sunSize = 15;
+let goop = 0;
 
-    requestAnimationFrame(assignment1);
+requestAnimationFrame(gaga);
+function gaga() {
+// Still Image
+ctx.clearRect(0, 0, cnv.width, cnv.height);
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, 1000, 300);
+ctx.fillStyle = "rgb(255, 0, 0)";
+ctx.beginPath();
+ctx.arc(200, 300, 15, 0, 2 * Math.PI);
+ctx.fill();
+ctx.fillStyle = "green";
+ctx.fillRect(0, 300, 1000, 100);
+ctx.drawImage(cloudImg, 150, 125);
+ctx.drawImage(cloudImg, 175, 100);
+requestAnimationFrame(gaga);
+}
+
+document.getElementById("btn1").addEventListener("click", funky);
+
+function funky() {
+    requestAnimationFrame(assignment1)
+    function assignment1() {
+
+        // Animations
+        x--;
+        x2++;
+        if (sunY > 150) {
+            sunY--;
+        }
+        if (sunSize < 50) {
+            sunSize += 0.23;
+        }
+        if (goop < 255) {
+            goop += 1.5;
+        }
+
+        // Sky
+        ctx.clearRect(0, 0, cnv.width, cnv.height);
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, 1000, 300);
+
+        // Sun
+        ctx.fillStyle = `rgb(255, ${goop}, 0)`;
+        ctx.beginPath();
+        ctx.arc(200, sunY, sunSize, 0, 2 * Math.PI);
+        ctx.fill();
+
+        // Grass
+        ctx.fillStyle = "green";
+        ctx.fillRect(0, 300, 1000, 100);
+
+        // Left Cloud
+        ctx.drawImage(cloudImg, x, 125);
+
+        // Right Cloud
+        ctx.drawImage(cloudImg, x2, 100);
+
+        requestAnimationFrame(assignment1);
+    }
 }
